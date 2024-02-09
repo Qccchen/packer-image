af8d3fd7a4fe949231aef3c7e737745fe5586934
QianChen <chen.qian6@northeastern.edu>
2024-02-09 01:44:58 -0800

    Update assign02

--- users.js
+++ users.js
@@ -2,9 +2,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || 'csye6225',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || '99chenqian99',
     {
         host: 'localhost',
         dialect: 'mysql',
