8977c6aa899ccb3554e6a9d6302fc1a5728f57e4
QianChen <chen.qian6@northeastern.edu>
2024-02-09 00:57:04 -0800

    Add assign02

---
+++ users.js
@@ -0,0 +1,45 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+    }
+);
+
+const Users = sequelize.define('users', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            is: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        set(value) {
+            this.setDataValue('password', bcrypt.hashSync(value, 10))
+        },
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+module.exports = { Users, sequelize };
