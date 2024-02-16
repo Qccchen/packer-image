2d109ae1411ea7e2c339a0b0149b856a6cc8e72d
ChenChenChen99 <115513462+ChenChenChen99@users.noreply.github.com>
2024-02-16 01:42:43 -0800

    Assign03 (#13)
    * Update tests and workflow
    
    * Update user.test.yml
    
    * Update users.js
    
    * Update package.json
    
    * Update assign
    
    * Update user.test.yml
    
    ---------
    
    Co-authored-by: QianChen <chen.qian6@northeastern.edu>

--- users.js
+++ users.js
@@ -2,9 +2,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || 'csye6225',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '1234567',
+    'test_db',
+    'root',
+    '99chenqian99',
     {
         host: 'localhost',
         dialect: 'mysql',
